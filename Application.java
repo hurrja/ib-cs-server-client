5ac35020b08898d7984b7c59ea39e32286f26fe8
Jarmo Hurri <jarmo.hurri@iki.fi>
2024-05-19 12:59:44 +0300

    * role selection * ip address if role is server

--- Application.java
+++ Application.java
@@ -1,9 +1,37 @@
+import java.net.InetAddress;
+
 public class Application
 {
   public Application ()
   {
-    RoleWindow roleWindow = new RoleWindow (this);
+    roleWindow = new RoleWindow (this);
+    role = Role.Server;
+    roleWindow.showRole (role);
   }
 
-  public Role role;
+  public String getMyIP ()
+  {
+    String myIP = "";
+    
+    try
+    {
+      myIP = InetAddress.getLocalHost().getHostAddress();
+    }
+    catch (Exception e)
+    {
+      System.out.println ("unable to get IP address, exception [ " + e + " ]");
+      System.exit (1);
+    }
+
+    return myIP;
+  }
+
+  public void setRole (Role role)
+  {
+    this.role = role;
+    roleWindow.showRole (role);
+  }
+  
+  private Role role;
+  private RoleWindow roleWindow; 
 }
--- RoleWindow.java
+++ RoleWindow.java
@@ -13,6 +13,12 @@ public class RoleWindow extends JFrame
 
     roleComboBox = new JComboBox<> (Role.values ());
     add (roleComboBox);
+    roleComboBox.addActionListener ((ActionEvent e) ->
+                                    application.setRole ((Role) roleComboBox.getSelectedItem ()));
+    
+    ipTextField = new JTextField ();
+    ipTextField.setColumns (IP_ADDRESS_MAX_LENGTH);
+    add (ipTextField);
     JButton okButton = new JButton ("OK");
     okButton.addActionListener ((ActionEvent e) -> System.exit (1));
     add (okButton);
@@ -25,6 +31,23 @@ public class RoleWindow extends JFrame
     
   }
 
+  public void showRole (Role role)
+  {
+    roleComboBox.setSelectedItem (role);
+    if (role == Role.Server)
+    {
+      ipTextField.setText (application.getMyIP ());
+      ipTextField.setEditable (false);
+    }
+    else
+    {
+      ipTextField.setText ("");
+      ipTextField.setEditable (true);
+    }
+  }
+  
   private Application application;
-  JComboBox<Role> roleComboBox;
+  private JComboBox<Role> roleComboBox;
+  private JTextField ipTextField;
+  final int IP_ADDRESS_MAX_LENGTH = 15;
 }
