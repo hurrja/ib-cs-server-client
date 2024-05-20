32a48279e664976678bd2515f6448ea99812173b
Jarmo Hurri <jarmo.hurri@iki.fi>
2024-05-20 11:04:05 +0300

    * getting ready to accept

--- Server.java
+++ Server.java
@@ -1,10 +1,13 @@
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server extends Application
 {
   public Server (Starter starter)
   {
     super (starter, Role.Server);
+    clientSockets = new Socket [NUM_CLIENTS];
+    curNumClients = 0;
   }
 
   public void run ()
@@ -18,6 +21,17 @@ public class Server extends Application
       System.out.println ("unable to open server socket, exception [ " + e + " ]");
       System.exit (1);
     }
+    acceptClient ();
   }
+
+  private void acceptClient ()
+  {
+    assert (curNumClients < NUM_CLIENTS);
+    setStatusText ("accepting");
+  }
+  
   ServerSocket serverSocket;
+  final int NUM_CLIENTS = 2;
+  int curNumClients;
+  Socket[] clientSockets;
 }
