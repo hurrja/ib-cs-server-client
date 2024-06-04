354c099a6f8300e44db4332127cc1850c2a9e768
Jarmo Hurri <jarmo.hurri@iki.fi>
2024-06-04 11:18:28 +0300

    * send and receive

--- Client.java
+++ Client.java
@@ -28,10 +28,34 @@ public class Client extends Application
   
   public void send ()
   {
+    try
+    {
+      serverOutputStream.writeObject (new Data (1));
+    }
+    catch (Exception e)
+    {
+      System.out.println ("unable to send data, exception [ " + e + " ]");
+      System.exit (1);
+    }
+
+    setStatusText ("sent reply to server, waiting for more requests");
+    deactivateSend ();
+    SwingUtilities.invokeLater (() -> receive ());
   }
 
   public void receive ()
   {
+    try
+    {
+      Data data = (Data) serverInputStream.readObject ();
+    }
+    catch (Exception e)
+    {
+      System.out.println ("unable to receive data, exception [ " + e + " ]");
+      System.exit (1);
+    }
+    setStatusText ("received request from server, ready to reply");
+    activateSend ();
   }
   
   protected ObjectInputStream serverInputStream;
